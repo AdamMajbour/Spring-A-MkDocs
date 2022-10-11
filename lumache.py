12821a63f20cf814fd3548f0a3f4908bbf2e7912
adam.majbour <adam.majbour@goldenore.com>
2022-10-11 23:29:34 +0100

    mkd

--- lumache.py
+++ lumache.py
@@ -1,11 +1,15 @@
 """
 Lumache - Python library for cooks and food lovers.
+
 This is a Python docstring, we can use Markdown syntax here because
 our API documentation library understands it (mkdocstrings).
+
     # Import lumache
     import lumache
+
     # Call its only function
     get_random_ingredients(kind=["cheeses"])
+
 """
 
 __version__ = "0.1.0"
@@ -20,6 +24,7 @@ class InvalidKindError(Exception):
 def get_random_ingredients(kind=None):
     """
     Return a list of random ingredients as strings.
+
     :param kind: Optional "kind" of ingredients.
     :type kind: list[str] or None
     :raise lumache.InvalidKindError: If the kind is invalid.
